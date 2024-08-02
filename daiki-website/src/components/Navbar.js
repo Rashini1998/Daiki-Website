712b7b81e50ccfcfca769a47e5a7a2970b10f27c
Rashini Sathsarani Gamalath <gamalathrashini@gmail.com>
2024-08-02 17:24:51 +0530

    add responsiveness

--- daiki-website/src/components/Navbar.js
+++ daiki-website/src/components/Navbar.js
@@ -1,3 +1,80 @@
+// import React, { useState } from 'react';
+// import { Link } from 'react-router-dom';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// const Navbar = () => {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const toggleOffcanvas = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <>
+//       <nav className="navbar navbar-expand-lg navbar-light bg-white">
+//         <div className="container-fluid d-flex align-items-center">
+//           <button
+//             className="navbar-toggler"
+//             type="button"
+//             onClick={toggleOffcanvas}
+//             aria-controls="offcanvasNavbar"
+//             aria-expanded={isOpen}
+//             aria-label="Toggle navigation"
+//           >
+//             <span className="navbar-toggler-icon"></span>
+//           </button>
+//           <div className="collapse navbar-collapse">
+//             <ul className="navbar-nav ms-auto">
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/">Home</Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/services">Services</Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/about">About Us</Link>
+//               </li>
+//               <li className="nav-item">
+//                 <Link className="nav-link" to="/contact">Contact</Link>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+
+//       <div className={`offcanvas offcanvas-end ${isOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar">
+//         <div className="offcanvas-header">
+//           <button
+//             type="button"
+//             className="btn-close text-reset"
+//             aria-label="Close"
+//             onClick={toggleOffcanvas}
+//           ></button>
+//         </div>
+//         <div className="offcanvas-body">
+//           <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/" onClick={toggleOffcanvas}>Home</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/services" onClick={toggleOffcanvas}>Services</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/about" onClick={toggleOffcanvas}>About Us</Link>
+//             </li>
+//             <li className="nav-item">
+//               <Link className="nav-link" to="/contact" onClick={toggleOffcanvas}>Contact</Link>
+//             </li>
+//           </ul>
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -13,7 +90,7 @@ const Navbar = () => {
   return (
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container-fluid d-flex align-items-center">
+        <div className="container-fluid">
           <button
             className="navbar-toggler"
             type="button"
@@ -24,51 +101,34 @@ const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/services">Services</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">About Us</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/contact">Contact</Link>
-              </li>
-            </ul>
+          <div className={`offcanvas offcanvas-end ${isOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar">
+            <div className="offcanvas-header">
+              <button
+                type="button"
+                className="btn-close text-reset"
+                aria-label="Close"
+                onClick={toggleOffcanvas}
+              ></button>
+            </div>
+            <div className="offcanvas-body">
+              <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
+                <li className="nav-item">
+                  <Link className="nav-link" to="/" onClick={toggleOffcanvas}>Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/services" onClick={toggleOffcanvas}>Services</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/about" onClick={toggleOffcanvas}>About Us</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/contact" onClick={toggleOffcanvas}>Contact</Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
-
-      <div className={`offcanvas offcanvas-end ${isOpen ? 'show' : ''}`} tabIndex="-1" id="offcanvasNavbar">
-        <div className="offcanvas-header">
-          <button
-            type="button"
-            className="btn-close text-reset"
-            aria-label="Close"
-            onClick={toggleOffcanvas}
-          ></button>
-        </div>
-        <div className="offcanvas-body">
-          <ul className="navbar-nav justify-content-end flex-grow-1 pe-3">
-            <li className="nav-item">
-              <Link className="nav-link" to="/" onClick={toggleOffcanvas}>Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/services" onClick={toggleOffcanvas}>Services</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about" onClick={toggleOffcanvas}>About Us</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/contact" onClick={toggleOffcanvas}>Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </div>
     </>
   );
 };
